025c842d66c55d3bb239c1055484efb37fa07e1c
darryl west <darryl.west@RainCitySoftware.com>
2016-09-22 12:43:40 -0700

    added verify secure option switch

--- test/delegates/MiddlewareDelegateTests.js
+++ test/delegates/MiddlewareDelegateTests.js
@@ -18,6 +18,7 @@ describe('MiddlewareDelegate', function() {
         opts.log = new MockLogger.createLogger('MiddlewareDelegate');
         opts.appkey = 'ae3b1d1c-7a44-45f6-82f5-0a4eb789ae10';
         opts.acceptedProtocols = [ 'http', 'https' ];
+        opts.verifySecure = true;
 
         return opts;
     };
